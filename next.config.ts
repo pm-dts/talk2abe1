f8fb3dfb62cfa7bc2797587c1321a25311e9d7c4
Mainak Saha <mainaksaha@Mainaks-MacBook-Air.local>
2026-08-14 12:28:57 +0530

    Update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  output: "export",
   // allowedDevOrigins: ["10.30.222.76"],
   images: {
     unoptimized: true,
